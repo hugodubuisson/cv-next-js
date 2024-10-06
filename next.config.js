7ce731259444c9e0bb80a0b76fd41f5b9564bac2
Hugo Dubuisson <hugo.dubuisson59@gmail.com>
2024-10-06 20:12:38 +0200

    Changement du next.config.js

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
+    basePath: '/cv-next-js',
+    assetPrefix: '/cv-next-js/',
+};
+
+module.exports = nextConfig;
