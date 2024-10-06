64cf219295c6b523a4ac32dcbfed1b585945d549
Hugo Dubuisson <hugo.dubuisson59@gmail.com>
2024-10-06 20:28:47 +0200

    Mise à niveau du next.config

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export',
-    basePath: '/cv-next-js',
-    assetPrefix: '/cv-next-js/',
-};
-
-module.exports = nextConfig;
