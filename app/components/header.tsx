4e26f676dabf0924a2d31d90092f3a2364c45a00
Hugo Dubuisson <hugo.dubuisson59@gmail.com>
2024-10-07 18:56:35 +0200

    Nouvelle image

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,14 +1,30 @@
-import React from 'react';
-import Image from 'next/image';
+import React from "react";
+import Image from "next/image";
 
 export default function Header() {
-    return (
-        <header>
-            <Image src="./images/profile.jpg" alt="Profile" className="profile-image" width={150} height={150} />
-            <div className="profile-info">
-                <h1>Dubuisson Hugo</h1>
-                <p>Jeune développeur en alternance chez <a href="https://www.portailpro.net/" target="_blank" rel="noopener noreferrer">Portailpro</a> en développement de site web en Symfony</p>
-            </div>
-        </header>
-    );
+  return (
+    <header>
+      <Image
+        src="./images/profile2.jpg"
+        alt="Profile"
+        className="profile-image"
+        width={150}
+        height={150}
+      />
+      <div className="profile-info">
+        <h1>Dubuisson Hugo</h1>
+        <p>
+          Jeune développeur en alternance chez{" "}
+          <a
+            href="https://www.portailpro.net/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Portailpro
+          </a>{" "}
+          en développement de site web en Symfony
+        </p>
+      </div>
+    </header>
+  );
 }
