3f90964f035c73baa42a393719200934f261bddf
Hugo Dubuisson <hugo.dubuisson59@gmail.com>
2024-10-07 18:45:59 +0200

    Reduction taille image profile

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 export default function Header() {
     return (
         <header>
-            <Image src="./images/profile.svg" alt="Profile" className="profile-image" width={150} height={150} />
+            <Image src="./images/profile.jpg" alt="Profile" className="profile-image" width={150} height={150} />
             <div className="profile-info">
                 <h1>Dubuisson Hugo</h1>
                 <p>Jeune développeur en alternance chez <a href="https://www.portailpro.net/" target="_blank" rel="noopener noreferrer">Portailpro</a> en développement de site web en Symfony</p>
