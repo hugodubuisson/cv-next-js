c35f687e9346a1cf0177862d86049e5e3d81623f
Hugo Dubuisson <hugo.dubuisson59@gmail.com>
2024-10-16 18:33:30 +0200

    Ajout du lazy

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -2,29 +2,30 @@ import React from "react";
 import Image from "next/image";
 
 export default function Header() {
-  return (
-    <header>
-      <Image
-        src="./images/profile3.jpg"
-        alt="Profile"
-        className="profile-image"
-        width={150}
-        height={150}
-      />
-      <div className="profile-info">
-        <h1>Dubuisson Hugo</h1>
-        <p>
-          Jeune développeur en alternance chez{" "}
-          <a
-            href="https://www.portailpro.net/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Portailpro
-          </a>{" "}
-          en développement de site web en Symfony
-        </p>
-      </div>
-    </header>
-  );
+    return (
+        <header>
+            <Image
+                src="./images/profile3.jpg"
+                alt="Profile"
+                className="profile-image"
+                width={150}
+                height={150}
+                loading="lazy"
+            />
+            <div className="profile-info">
+                <h1>Dubuisson Hugo</h1>
+                <p>
+                    Jeune développeur en alternance chez{" "}
+                    <a
+                        href="https://www.portailpro.net/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Portailpro
+                    </a>{" "}
+                    en développement de site web en Symfony
+                </p>
+            </div>
+        </header>
+    );
 }
