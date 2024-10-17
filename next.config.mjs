b2472147860d4c696f5d5a71d707d92905562717
Hugo Dubuisson <hugo.dubuisson59@gmail.com>
2024-10-17 15:37:23 +0200

    Modif de mon next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    images: {
-        unoptimized: true,
-    },
-    async headers() {
-        return [
-            {
-                source: '/(.*)',
-                headers: [
-                    {
-                        key: 'Cache-Control',
-                        value: 'public, max-age=31536000, immutable', // Cache pour 1 an
-                    },
-                ],
-            },
-        ];
-    },
 };
 
 export default nextConfig;
