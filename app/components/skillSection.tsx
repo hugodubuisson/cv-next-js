fe3d20dbcc95fa82d3c84d4f69c53d935a2bac1d
Hugo Dubuisson <hugo.dubuisson59@gmail.com>
2024-10-18 10:01:22 +0200

    Changement pour les images

--- app/components/skillSection.tsx
+++ app/components/skillSection.tsx
@@ -11,14 +11,6 @@ const SkillsSection: React.FC = () => {
             <h2 className={styles.skillsTitle}>Développement Web</h2>
             <div className={styles.skillsGrid}>
                 <div className={styles.skillCard}>
-                    <h3 className={styles.skillCardTitle}>HTML</h3>
-                    <Image src="./images/html.svg" alt="HTML" width={64} height={64} className={styles.skillCardImage} loading="lazy" />
-                </div>
-                <div className={styles.skillCard}>
-                    <h3 className={styles.skillCardTitle}>CSS</h3>
-                    <Image src="./images/css-3.svg" alt="CSS" width={64} height={64} className={styles.skillCardImage} loading="lazy" />
-                </div>
-                <div className={styles.skillCard}>
                     <h3 className={styles.skillCardTitle}>JavaScript</h3>
                     <Image src="./images/script-java.svg" alt="JavaScript" width={64} height={64} className={styles.skillCardImage} loading="lazy" />
                 </div>
